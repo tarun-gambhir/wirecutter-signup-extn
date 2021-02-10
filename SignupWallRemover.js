1857cb1efa64d76b5d94803db4d09f862e4bcea3
tarung <tarung@surveymonkey.com>
2021-02-09 20:13:33 -0800

    Add manifest and functionality

---
+++ SignupWallRemover.js
@@ -0,0 +1,6 @@
+const body = document.querySelector('body');
+
+setTimeout(() => {
+    body.style.overflow = 'scroll';
+    document.getElementById('modal-portal-regiwall').style.display = 'none';
+}, 150);
